726537b3e42447dd33a7698cfb9140514718ec5c
Melissa Malpica <Malpicadz@gmail.com>
2014-11-22 15:12:16 -0500

    Attempting to get login to work with SimpleLogin from FireBase

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,8 +1,21 @@
 myApp.controller('RegistrationController', 
-  function($scope, $location) {
-  
+  function($scope, $firebaseSimpleLogin, $location) {
+
+  	var ref = new Firebase('https://goldenstrawberries.firebaseio.com/');
+    var simpleLogin = $firebaseSimpleLogin(ref);
+
   $scope.login = function() {
-    $location.path('/meetings');
+
+  	simpleLogin.$login('password', {
+
+  		email: $scope.user.email,
+  		password: $scope.user.password
+  	}).then (function(user){
+
+  	$location.path('/meetings');
+
+  	});
+
   } //login
 
   $scope.register = function() {
